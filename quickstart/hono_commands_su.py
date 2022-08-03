04af04dc6289d02ce9668b96b54541e7916d070c
Gabriela Tsvetomirova Yoncheva <Gabriela.Yoncheva@bosch.io>
2022-08-03 10:29:42 +0300

    Provide How-to guide for installing a Debian package via Software Update (#65)
    [#12] Provide How-to guide for Software Update
    
    - created an application that spins up a hello Debian package at the edge
    - added a guide to the Eclipse Kanto Documentation
    - updated Software Update concept page with link to the how to guide
    - created bash script for installing hello Debian package
    
    Signed-off-by: Gabriela Yoncheva <Gabriela.Yoncheva@bosch.io>

---
+++ quickstart/hono_commands_su.py
@@ -0,0 +1,145 @@
+# Copyright (c) 2022 Contributors to the Eclipse Foundation
+#
+# See the NOTICE file(s) distributed with this work for additional
+# information regarding copyright ownership.
+#
+# This program and the accompanying materials are made available under the
+# terms of the Eclipse Public License 2.0 which is available at
+# http://www.eclipse.org/legal/epl-2.0
+#
+# SPDX-License-Identifier: EPL-2.0
+
+import getopt
+import json
+import os
+import signal
+import sys
+import threading
+import time
+import uuid
+
+from string import Template
+from proton import Message
+from proton.handlers import MessagingHandler
+from proton.reactor import Container
+
+ditto_live_inbox_msg_template = Template("""
+{
+    "topic": "$namespace/$name/things/live/messages/install",
+    "headers": {
+        "content-type": "application/json",
+        "correlation-id": "$correlation_id",
+        "response-required": true
+    },
+    "path": "/features/SoftwareUpdatable/inbox/messages/install",
+    "value": {
+        "softwareModules": [{
+            "softwareModule": {
+                "name": "install-hello",
+                "version": "1.0.0"
+            },
+            "artifacts": [{
+                "checksums": {
+                    "SHA256": "03a105509663680d6d5db0e2b5939a77fab68429fca4dd5d181924a73e82b5d9"
+                },
+                "download": {
+                    "HTTPS": {
+                        "url": "https://github.com/eclipse-kanto/kanto/raw/main/quickstart/install_hello.sh"
+                    }
+                },
+                "filename": "install.sh",
+                "size": 	424
+            }]
+        }],
+        "correlationId": "$correlation_id"
+    }
+}
+""")
+
+
+class CommandResponsesHandler(MessagingHandler):
+    def __init__(self, server, address):
+        super(CommandResponsesHandler, self).__init__()
+        self.server = server
+        self.address = address
+
+    def on_start(self, event):
+        conn = event.container.connect(self.server, user="consumer@HONO", password="verysecret")
+        event.container.create_receiver(conn, self.address)
+        print('[connected]')
+
+    def on_message(self, event):
+        print('[got response]')
+        response = json.loads(event.message.body)
+        print(json.dumps(response, indent=2))
+        if response["status"] == 204:
+            print('[ok]', "su")
+        else:
+            print('[error]')
+        os.kill(os.getpid(), signal.SIGINT)
+
+
+class CommandsInvoker(MessagingHandler):
+    def __init__(self, server, address):
+        super(CommandsInvoker, self).__init__()
+        self.server = server
+        self.address = address
+
+    def on_start(self, event):
+        conn = event.container.connect(self.server, sasl_enabled=True, allowed_mechs="PLAIN", allow_insecure_mechs=True,
+                                       user="consumer@HONO", password="verysecret")
+        event.container.create_sender(conn, self.address)
+
+    def on_sendable(self, event):
+        print('[sending command]')
+        correlation_id = str(uuid.uuid4())
+        namespaced_id = device_id.split(':', 1)
+
+        payload = ditto_live_inbox_msg_template.substitute(namespace=namespaced_id[0], name=namespaced_id[1],
+                                                           correlation_id=correlation_id)
+        print(json.dumps(json.loads(payload), indent=2))
+        msg = Message(body=payload, address='{}/{}'.format(address, device_id),
+                      content_type="application/json",
+                      subject="su", reply_to=reply_to_address, correlation_id=correlation_id, id=str(uuid.uuid4()))
+        event.sender.send(msg)
+        event.sender.close()
+        event.connection.close()
+        print('[sent]')
+
+
+# Parse command line args
+options, reminder = getopt.getopt(sys.argv[1:], 't:d:')
+opts_dict = dict(options)
+tenant_id = os.environ.get("TENANT") or opts_dict['-t']
+device_id = os.environ.get("DEVICE_ID") or opts_dict['-d']
+
+
+# AMQP global configurations
+uri = 'amqp://hono.eclipseprojects.io:15672'
+address = 'command/{}'.format(tenant_id)
+reply_to_address = 'command_response/{}/replies'.format(tenant_id)
+
+print('[starting] demo software update app for tenant [{}], device [{}] at [{}]'.format(tenant_id, device_id, uri))
+
+# Create command invoker and handler
+response_handler = Container(CommandResponsesHandler(uri, reply_to_address))
+commands_invoker = Container(CommandsInvoker(uri, address))
+thread = threading.Thread(target=lambda: response_handler.run(), daemon=True)
+thread.start()
+# Give it some time to link
+time.sleep(2)
+# Send the command
+commands_invoker.run()
+
+
+def handler(signum, frame):
+    print('[stopping] demo software update app for tenant [{}], device [{}] at [{}]'.format(tenant_id, device_id, uri))
+    response_handler.stop()
+    thread.join(timeout=5)
+    print('[stopped]')
+    exit(0)
+
+
+signal.signal(signal.SIGINT, handler)
+while True:
+    pass
---
+++ web/site/content/docs/how-to-guides/update-software.md
@@ -0,0 +1,73 @@
+---
+title: "Update software"
+type: docs
+description: >
+    Install a Debian package on your edge device.
+weight: 1
+---
+
+Following the steps below you will install a{{% refn "https://www.gnu.org/software/hello/" %}}`hello`{{% /refn %}}
+Debian package via a publicly available Eclipse Hono sandbox using Eclipse Kanto.
+A couple of simple Eclipse Hono northbound business applications written in Python are provided to explore
+the capabilities for remotely installing and monitoring.
+On the edge side, a basic
+{{% refn "https://github.com/eclipse-kanto/kanto/blob/main/quickstart/install_hello.sh" %}}`install_hello.sh`{{% /refn %}}
+script will be downloaded and executed.
+
+### Before you begin
+
+To ensure that your edge device is capable to execute the steps in this guide, you need:
+
+* Debian-based linux distribution and the `apt` command line tool
+* If you don't have an installed and running Eclipse Kanto, follow {{% relrefn "install" %}} Install Eclipse Kanto {{% /relrefn %}}
+* If you don't have a connected Eclipse Kanto to Eclipse Hono sandbox,
+  follow {{% relrefn "hono" %}} Explore via Eclipse Hono {{% /relrefn %}}
+
+* The {{% refn "https://github.com/eclipse-kanto/kanto/blob/main/quickstart/hono_commands_su.py" %}} 
+  software update application {{% /refn %}}
+
+  Navigate to the `quickstart` folder where the resources from the {{% relrefn "hono" %}} Explore via Eclipse Hono {{% /relrefn %}}
+  guide are located and execute the following script:
+
+  ```shell
+  wget https://github.com/eclipse-kanto/kanto/raw/main/quickstart/hono_commands_su.py
+  ```
+* Executing `hello` in the terminal will return that the command is not found
+
+### Install Debian package
+
+To explore the software management, we will use two Python scripts to install and monitor the `hello` Debian package.
+The location where the Python applications will run does not have to be your edge device as they communicate remotely
+with Eclipse Hono only.
+
+First, start the monitoring application that requires the configured Eclipse Hono tenant (`-t`) and will print all
+received events triggered by the device:
+
+```shell
+python3 hono_events.py -t demo
+```
+
+In another terminal, we are ready to spin up a `hello` Debian package at the edge via executing the second application
+that requires the Eclipse Hono tenant (`-t`) and the device identifier (`-d`):
+
+```shell
+python3 hono_commands_su.py -t demo -d demo:device
+```
+
+### Verify
+
+You can check out that the new package is installed on your edge device via executing:
+
+```shell
+hello
+```
+
+The command now displays: `Hello, world!`
+
+### Clean up
+
+The installed `hello` Debian package can be removed via executing:
+
+```shell
+sudo apt remove hello
+```
