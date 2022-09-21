058b6190ad29cec57305bef9babf4d991178430c
Gabriela Yoncheva <Gabriela.Yoncheva@bosch.io>
2022-09-21 15:04:29 +0300

    [#52] Provide How-to Guide for Suite Bootstrapping - updated license copyright header
    Signed-off-by: Gabriela Yoncheva <Gabriela.Yoncheva@bosch.io>

--- quickstart/post_script.sh
+++ quickstart/post_script.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-#
 # Copyright (c) 2022 Contributors to the Eclipse Foundation
 #
 # See the NOTICE file(s) distributed with this work for additional
@@ -7,7 +6,9 @@
 #
 # This program and the accompanying materials are made available under the
 # terms of the Eclipse Public License 2.0 which is available at
-# http://www.eclipse.org/legal/epl-2.0
+# https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
+# which is available at https://www.apache.org/licenses/LICENSE-2.0.
 #
-# SPDX-License-Identifier: EPL-2.0
+# SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
+
 systemctl restart suite-connector.service
